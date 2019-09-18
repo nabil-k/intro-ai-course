acde42c866b2be95ffdb007ae1eb06f9a4cd992e
nabilkhalil <duomaster01@gmail.com>
2019-09-17 17:10:42 -0700

    finished kmc example

--- K_Means_Clustering/kmc.py
+++ K_Means_Clustering/kmc.py
@@ -15,6 +15,22 @@ rabbits_data = pd_data.values # assigning the values to rabbits_data
 x = rabbits_data[:, 1]
 y = rabbits_data[:, 2]
 centroids = []
+
+# a is an array that stores other arrays that hold x and y values
+def mean_of_points(a):
+    sum_x = 0
+    sum_y = 0
+    for c in a:
+        sum_x += c[0]
+        sum_y += c[1]
+    
+    if len(a) != 0 :
+        mean_x = sum_x / len(a)
+        mean_y = sum_y / len(a)
+        return [mean_x, mean_y]
+    
+    return None
+
 def K_Means(k):
   
     cluster_members = []
@@ -23,22 +39,25 @@ def K_Means(k):
         centroid = [random.randint(0,12), random.randint(0,12)]
         centroids.append(centroid)
         cluster_members.append([])
+
+    print(centroids)
     
     counter = 0
-    #while counter < 100:
-    for i in range(int(x.size)):
-        distances = []
-        for g in range(k):
-            cur_c = centroids[g] # current centroid
-            distance = ((cur_c[0]-x[i])**2+(cur_c[1]-y[i])**2)**(1/2)
-            distances.append(distance)
-        clos_c_i = distances.index(min(distances)) # closest centroid index
-        cluster_members[clos_c_i].append([x[i],y[i]])
+    while counter < 100:
+        for i in range(int(x.size)):
+            distances = []
+            for g in range(k):
+                cur_c = centroids[g] # current centroid
+                distance = ((cur_c[0]-x[i])**2+(cur_c[1]-y[i])**2)**(1/2)
+                distances.append(distance)
+            clos_c_i = distances.index(min(distances)) # closest centroid index
+            cluster_members[clos_c_i].append([x[i],y[i]])
 
-    print(cluster_members)
-
-        
-    counter += 1
+        for i in range(len(centroids)):
+            new_centroid = mean_of_points(cluster_members[i])
+            if new_centroid != None:
+                centroids[i] = new_centroid
+        counter += 1    
 
 K_Means(2)
 
@@ -48,7 +67,7 @@ plt.plot(x[int(x.size/2):], y[int(y.size/2):], '.')
 
 print(centroids)
 centroids = np.array(centroids)
-plt.plot(centroids[:, 0], centroids[:, 1], '.')
+plt.plot(centroids[:, 0], centroids[:, 1], 'g.', markersize=12)
 
 plt.ylabel('speed (m/s)')
 plt.xlabel('weight (lb)')
@@ -58,6 +77,3 @@ run_time = time.time() - start_time
 print("run time:", run_time, "s")
 
 plt.show() # shows the graph
-
-
-
